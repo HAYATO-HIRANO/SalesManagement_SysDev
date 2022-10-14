a48df06c7e3bdadc29731376c479a30497af29ca
海都 山之内 <c1403@oic.jp>
2022-10-14 16:32:59 +0900

    商品管理作成

--- SalesManagement_SysDev/FormProduct.cs
+++ SalesManagement_SysDev/FormProduct.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SalesManagement_SysDev
 {
-    public partial class Form1 : Form
+    public partial class FormProduct : Form
     {
-        public Form1()
+        public FormProduct()
         {
             InitializeComponent();
         }
