5eef47091ccad3d094f44a26e622da29975fd04b
海都 山之内 <c1403@oic.jp>
2022-10-14 16:42:05 +0900

    ｓ

--- SalesManagement_SysDev/FormProduct.Designer.cs
+++ SalesManagement_SysDev/FormProduct.Designer.cs
@@ -53,7 +53,7 @@ namespace SalesManagement_SysDev
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.bu = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -259,14 +259,14 @@ namespace SalesManagement_SysDev
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // bu
             // 
-            this.button2.Location = new System.Drawing.Point(592, 139);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 23;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.bu.Location = new System.Drawing.Point(592, 139);
+            this.bu.Name = "bu";
+            this.bu.Size = new System.Drawing.Size(75, 23);
+            this.bu.TabIndex = 23;
+            this.bu.Text = "検索";
+            this.bu.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -283,7 +283,7 @@ namespace SalesManagement_SysDev
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1902, 1033);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bu);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox10);
@@ -343,7 +343,7 @@ namespace SalesManagement_SysDev
         private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button bu;
         private System.Windows.Forms.Button button3;
     }
 }
