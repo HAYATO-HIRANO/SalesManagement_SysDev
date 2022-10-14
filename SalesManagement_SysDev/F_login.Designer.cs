d0b8edef3380b1329e960abdbe8fd9101a42f551
C1195@oic.jp <c1195@oic.jp>
2022-10-14 15:03:27 +0900

    テスト

--- SalesManagement_SysDev/F_login.Designer.cs
+++ SalesManagement_SysDev/F_login.Designer.cs
@@ -32,14 +32,15 @@
             this.Lbl_tag = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_CleateDabase
             // 
-            this.btn_CleateDabase.Location = new System.Drawing.Point(881, 486);
-            this.btn_CleateDabase.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_CleateDabase.Location = new System.Drawing.Point(1101, 583);
+            this.btn_CleateDabase.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btn_CleateDabase.Name = "btn_CleateDabase";
-            this.btn_CleateDabase.Size = new System.Drawing.Size(141, 61);
+            this.btn_CleateDabase.Size = new System.Drawing.Size(176, 73);
             this.btn_CleateDabase.TabIndex = 0;
             this.btn_CleateDabase.Text = "データベース生成";
             this.btn_CleateDabase.UseVisualStyleBackColor = true;
@@ -48,42 +49,53 @@
             // Lbl_tag
             // 
             this.Lbl_tag.AutoSize = true;
-            this.Lbl_tag.Location = new System.Drawing.Point(355, 201);
-            this.Lbl_tag.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_tag.Location = new System.Drawing.Point(444, 241);
+            this.Lbl_tag.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Lbl_tag.Name = "Lbl_tag";
-            this.Lbl_tag.Size = new System.Drawing.Size(280, 15);
+            this.Lbl_tag.Size = new System.Drawing.Size(335, 18);
             this.Lbl_tag.TabIndex = 1;
             this.Lbl_tag.Text = "このページはログイン画面として作成してください";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(260, 319);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(325, 383);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.Size = new System.Drawing.Size(62, 18);
             this.label1.TabIndex = 2;
             this.label1.Text = "平野隼";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(343, 319);
+            this.label2.Location = new System.Drawing.Point(429, 383);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.Size = new System.Drawing.Size(62, 18);
             this.label2.TabIndex = 3;
             this.label2.Text = "山之内";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(533, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "鈴東";
+            // 
             // F_Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.ClientSize = new System.Drawing.Size(1334, 674);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Lbl_tag);
             this.Controls.Add(this.btn_CleateDabase);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "F_Login";
             this.Text = "販売管理システムログイン画面";
             this.ResumeLayout(false);
@@ -97,6 +109,7 @@
         private System.Windows.Forms.Label Lbl_tag;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
