b2c24ee83fb41e381eb7802b8b3535ab02f4f423
C1302 <C1302@oic.osaka.jp>
2022-10-18 17:52:26 +0900

    テストボタン追加

--- SalesManagement_SysDev/FormMain.Designer.cs
+++ SalesManagement_SysDev/FormMain.Designer.cs
@@ -196,7 +196,7 @@ namespace SalesManagement_SysDev
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(81, 484);
+            this.button1.Location = new System.Drawing.Point(87, 488);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
