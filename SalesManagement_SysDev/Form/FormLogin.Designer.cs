9962ae8a9c8d7565314024a7802536e9526aa697
横山旭 <C1412@oic.jp>
2022-10-20 13:27:55 +0900

    ログイン画面

--- SalesManagement_SysDev/Form/FormLogin.Designer.cs
+++ SalesManagement_SysDev/Form/FormLogin.Designer.cs
@@ -33,9 +33,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxEmLogin = new System.Windows.Forms.TextBox();
+            this.textBoxEmPassword = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -93,42 +94,55 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "パスワード";
             // 
-            // textBox1
+            // textBoxEmLogin
             // 
-            this.textBox1.Location = new System.Drawing.Point(258, 143);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(168, 22);
-            this.textBox1.TabIndex = 6;
+            this.textBoxEmLogin.Location = new System.Drawing.Point(258, 143);
+            this.textBoxEmLogin.Name = "textBoxEmLogin";
+            this.textBoxEmLogin.Size = new System.Drawing.Size(168, 22);
+            this.textBoxEmLogin.TabIndex = 6;
             // 
-            // textBox2
+            // textBoxEmPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(258, 197);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(168, 22);
-            this.textBox2.TabIndex = 7;
+            this.textBoxEmPassword.Location = new System.Drawing.Point(258, 197);
+            this.textBoxEmPassword.Name = "textBoxEmPassword";
+            this.textBoxEmPassword.Size = new System.Drawing.Size(168, 22);
+            this.textBoxEmPassword.TabIndex = 7;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(258, 301);
+            this.button2.BackColor = System.Drawing.Color.LightYellow;
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(202, 301);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(109, 33);
             this.button2.TabIndex = 8;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "ログイン";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.Location = new System.Drawing.Point(378, 301);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 33);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "閉じる";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // F_Login
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.Honeydew;
             this.ClientSize = new System.Drawing.Size(714, 452);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxEmPassword);
+            this.Controls.Add(this.textBoxEmLogin);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "F_Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "販売管理システムログイン画面";
@@ -145,9 +159,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxEmLogin;
+        private System.Windows.Forms.TextBox textBoxEmPassword;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
