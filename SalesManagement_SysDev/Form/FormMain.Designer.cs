cbca1f64c493e39e4dd9f845d8a93f0e7672b9ad
C1302 <C1302@oic.osaka.jp>
2022-10-21 14:26:30 +0900

    FormMainユーザー名移動

--- SalesManagement_SysDev/Form/FormMain.Designer.cs
+++ SalesManagement_SysDev/Form/FormMain.Designer.cs
@@ -68,7 +68,7 @@ namespace SalesManagement_SysDev
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(996, 59);
+            this.label3.Location = new System.Drawing.Point(914, 59);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 16);
             this.label3.TabIndex = 11;
@@ -79,7 +79,7 @@ namespace SalesManagement_SysDev
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(996, 27);
+            this.label2.Location = new System.Drawing.Point(875, 21);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 16);
             this.label2.TabIndex = 10;
