b1b6e62d945b7060ca726fcfa3b076e3778cb6d9
C1302 <C1302@oic.osaka.jp>
2022-11-04 16:42:47 +0900

    UserControl1社員管理に飛ぶ機能を追加

--- SalesManagement_SysDev/UserControlMain/UserControl1.cs
+++ SalesManagement_SysDev/UserControlMain/UserControl1.cs
@@ -25,7 +25,8 @@ namespace SalesManagement_SysDev.UserControlMain
 
         private void buttonEmployee_Click(object sender, EventArgs e)
         {
-
+            FormEmployee formEmployee = new FormEmployee();
+            formEmployee.Show();
         }
     }
 }
