23662d1f1dc2a6372ac2ba17aee228930578868b
C1302 <C1302@oic.osaka.jp>
2022-11-14 11:40:53 +0900

    Revert "FormProduct設定切り替え機能追加"
    This reverts commit 63d6d2b95f1db2fd7b3a47663addac3ee0294794.

--- SalesManagement_SysDev/Form/FormProduct.cs
+++ SalesManagement_SysDev/Form/FormProduct.cs
@@ -24,43 +24,7 @@ namespace SalesManagement_SysDev
 
         private void FormProduct_Load(object sender, EventArgs e)
         {
-            panelSetting.Visible = false;
-            userControlMaker1.Visible = false;
-            //userControlMajorClassification.Visible = false;
-            //userControlSmallClassification.Visible = false;
-        }
 
-        private void panelSetting_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void buttonSetting_Click(object sender, EventArgs e)
-        {
-            panelSetting.Visible = true;
-        }
-
-        private void buttonMaker_Click(object sender, EventArgs e)
-        {
-            labelProduct.Text = "メーカー管理";
-            userControlMaker1.Visible = true;
-            //userControlMajorClassification.Visible = false;
-            //userControlSmallClassification.Visible = false;
-        }
-
-        private void buttonEmployee_Click(object sender, EventArgs e)
-        {
-            //商品管理ボタン
-            labelProduct.Text = "商品管理";
-            panelSetting.Visible = false;
-            userControlMaker1.Visible = false;
-            //userControlMajorClassification.Visible = false;
-            //userControlSmallClassification.Visible = false;
-        }
-
-        private void buttonFormDel_Click(object sender, EventArgs e)
-        {
-            this.Close();
         }
     }
 }
