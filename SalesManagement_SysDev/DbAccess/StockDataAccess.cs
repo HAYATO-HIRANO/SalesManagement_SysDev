a839a3deaf56a2876a2870d4320b0bbb37fb62ef
C1195@oic.jp <c1195@oic.jp>
2022-11-15 18:01:54 +0900

    a

--- SalesManagement_SysDev/DbAccess/StockDataAccess.cs
+++ SalesManagement_SysDev/DbAccess/StockDataAccess.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace SalesManagement_SysDev.DbAccess
+namespace SalesManagement_SysDev//.DbAccess
 {
     class StockDataAccess
     {
+        ///////////////////////////////
+        //メソッド名：UpdateSmallClassData()
+        //引　数   ：在庫データ
+        //戻り値   ：True or False
+        //機　能   ：在庫データの更新
+        //          ：更新成功の場合True
+        //          ：更新失敗の場合False
+        ///////////////////////////////
+
+        public bool UpdateStockData(T_Stock updstock)
+        {
+            try
+            {
+                var context = new SalesManagement_DevContext();
+                var stock = context.T_Stocks.Single(x => x.StID == updstock.StID);
+                stock.PrID = updstock.PrID;
+                stock.StQuantity = updstock.StQuantity;
+                stock.StFlag = updstock.StFlag;
+
+
+
+                context.SaveChanges();
+                context.Dispose();
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "例外エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+
+
+        }
+
     }
 }
