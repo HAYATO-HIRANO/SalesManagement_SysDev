da0dd649342296b320662260373088309b6aad2a
田中皓基 <c1219@oic.jp>
2022-11-16 13:36:53 +0900

    受注　モジュール作成

--- SalesManagement_SysDev/DbAccess/OrderDataAccess.cs
+++ SalesManagement_SysDev/DbAccess/OrderDataAccess.cs
@@ -3,11 +3,100 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
 namespace SalesManagement_SysDev
 {
     class OrderDataAccess
     {
+        ///////////////////////////////
+        //メソッド名：AddOrderData()
+        //引　数   ：受注データ
+        //戻り値   ：True or False
+        //機　能   ：受注データの登録
+        //          ：登録成功の場合True
+        //          ：登録失敗の場合False
+        ///////////////////////////////
+        public bool AddOrderData(T_Order regOrder)
+        {
+            try
+            {
+                var context = new SalesManagement_DevContext();
+                context.T_Orders.Add(regOrder);
+                context.SaveChanges();
+                context.Dispose();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "例外エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        ///////////////////////////////
+        //メソッド名：AddOrderDetailData()
+        //引　数   ：受注詳細データ
+        //戻り値   ：True or False
+        //機　能   ：受注詳細データの登録
+        //          ：登録成功の場合True
+        //          ：登録失敗の場合False
+        ///////////////////////////////
+        public bool AddOrderDetailData(T_OrderDetail regOrderDetail)
+        {
+            try
+            {
+                var context = new SalesManagement_DevContext();
+                context.T_OrderDetails.Add(regOrderDetail);
+                context.SaveChanges();
+                context.Dispose();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "例外エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        ///////////////////////////////
+        //メソッド名：UpdateOrderData()
+        //引　数   :受注データ
+        //戻り値   ：True or False
+        //機　能   ：受注データの更新
+        //          ：更新成功の場合True
+        //          ：更新失敗の場合False
+        ///////////////////////////////
+        public bool UpdateOrderData(T_Order updOrder)
+        {
+            try
+            {
+                var context = new SalesManagement_DevContext();
+
+                var Order = context.T_Orders.Single(x => x.OrID == updOrder.OrID);
+                Order.SoID = updOrder.SoID;
+                Order.EmID = updOrder.EmID;
+                Order.ClID = updOrder.ClID;
+                Order.ClCharge = updOrder.ClCharge;
+                Order.OrDate = updOrder.OrDate;
+                Order.OrStateFlag = updOrder.OrStateFlag;
+                Order.OrFlag = updOrder.OrFlag;
+                Order.OrHidden = updOrder.OrHidden;
+               
+
+                context.SaveChanges();
+                context.Dispose();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "例外エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
 
     }
 }
