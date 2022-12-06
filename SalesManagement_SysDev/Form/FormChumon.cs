95a8ed2b2f6013c5c34a3a21f59741a731ea2d31
C1140@oic.osaka.jp <C1140@oic.osaka.jp>
2022-12-06 17:55:06 +0900

    1

--- SalesManagement_SysDev/Form/FormChumon.cs
+++ SalesManagement_SysDev/Form/FormChumon.cs
@@ -7,11 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace SalesManagement_SysDev
 {
     public partial class FormChumon : Form
     {
+        //メッセージ表示用クラスのインスタンス化
+        //MessageDsp messageDsp = new MessageDsp();
+        //データベース顧客テーブルアクセス用クラスのインスタンス化
+        ChumonDataAccess chumonDataAccess = new ChumonDataAccess();
+        //データベース営業所テーブルアクセス用クラスのインスタンス化
+        SalesOfficeDataAccess salesOfficeDataAccess = new SalesOfficeDataAccess();
+        //入力形式チェック用クラスのインスタンス化
+        DataInputFormCheck dataInputFormCheck = new DataInputFormCheck();
+        //データグリッドビュー用の顧客データ
+        private static List<M_ClientDsp> Client;
+        //コンボボックス用の営業所データ
+        private static List<M_SalesOffice> SalesOffice;
+
         public FormChumon()
         {
             InitializeComponent();
@@ -60,5 +72,76 @@ namespace SalesManagement_SysDev
                 return;
             }
         }
+
+
+        
+        ////////注文情報検索///////////
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            //妥当な注文データ取得
+            if (!GetValidDataAtSelect())
+                return;
+        }
+
+        ///////////////////////////////
+        //　＊.*.*.* 妥当な注文データ取得
+        //メソッド名：GetValidDataAtSlect()
+        //引　数   ：なし
+        //戻り値   ：true or false
+        //機　能   ：入力データの形式チェック
+        //          ：エラーがない場合True
+        //          ：エラーがある場合False
+        ///////////////////////////////
+        private bool GetValidDataAtSelect()
+        {
+            //注文IDの適否
+            if (!String.IsNullOrEmpty(textBoxChID.Text.Trim()))
+            {
+                //文字チェック
+                if (!dataInputFormCheck.CheckNumeric(textBoxChID.Text.Trim()))
+                {
+                    MessageBox.Show("注文IDは半角数値入力です", "入力確認", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBoxChID.Focus();
+                    return false;
+                }
+                //文字数
+                if (textBoxChID.TextLength > 6)
+                {
+                    MessageBox.Show("注文IDは6文字以下です", "入力確認", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBoxChID.Focus();
+                    return false;
+                }
+            }
+                //受注IDの適否
+                if (!String.IsNullOrEmpty(textBoxOrID.Text.Trim()))
+                {
+                    //文字チェック
+                    if (!dataInputFormCheck.CheckNumeric(textBoxOrID.Text.Trim()))
+                    {
+                        MessageBox.Show("受注IDは半角数値入力です", "入力確認", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBoxOrID.Focus();
+                        return false;
+                    }
+                    //文字数
+                    if (textBoxOrID.TextLength > 6)
+                    {
+                        MessageBox.Show("受注IDは6文字以下です", "入力確認", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBoxOrID.Focus();
+                        return false;
+                    }
+                }
+            return false;
+        }
+
+
+        private void buttonList_Click(object sender, EventArgs e)
+        {
+
+        }
     }
+
+
+
+
+
 }
