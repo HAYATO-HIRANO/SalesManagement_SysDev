0e798722fd2f39bb0bd3f63b775788fd448dc4be
C1182@oic.jp <C1182@oic.jp>
2022-12-15 14:54:20 +0900

    メーカ管理　訂正

--- SalesManagement_SysDev/UserControl/UserControlDetail/UserControlShipmentDetail.cs
+++ SalesManagement_SysDev/UserControl/UserControlDetail/UserControlShipmentDetail.cs
@@ -12,12 +12,22 @@ namespace SalesManagement_SysDev
 {
     public partial class UserControlShipmentDetail : UserControl
     {
+        // データベース出荷デーブルアクセス用クラスのインスタンス化
+        // データベース出荷詳細テーブルアクセス用クラスのインスタンス化
+        // データベース商品テーブルアクセス用クラスのインスタンス化
+        // 入力形式チェック用クラスのインスタンス化
+        //データグリッドビュー用の受注データ
         public UserControlShipmentDetail()
         {
             InitializeComponent();
         }
 
         private void UserControlShipmentDetail_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
         {
 
         }
