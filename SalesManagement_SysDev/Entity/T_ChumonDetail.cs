5ff5960a28bba49c81b135ea6d0d9dfa9e87505a
C1140@oic.osaka.jp <C1140@oic.osaka.jp>
2022-12-16 14:22:45 +0900

    注文管理

--- SalesManagement_SysDev/Entity/T_ChumonDetail.cs
+++ SalesManagement_SysDev/Entity/T_ChumonDetail.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel;
 
 namespace SalesManagement_SysDev
 {
@@ -16,5 +16,22 @@ namespace SalesManagement_SysDev
         public int PrID { get; set; }           //商品ID
         public int ChQuantity { get; set; }	    //数量
 
+    }
+    class T_ChumonDetailDsp
+    {
+        [DisplayName("注文ID")]
+        public int ChID { get; set; }
+        [DisplayName("注文詳細ID")]
+        public int ChDetailID { get; set; }
+        [DisplayName("商品ID")]
+        public int PrID { get; set; }
+        [DisplayName("商品名")]
+        public string PrName { get; set; }
+        [DisplayName("価格")]
+        public int Price { get; set; }
+        [DisplayName("数量")]
+        public int ChQuantity { get; set; }
+        [DisplayName("合計金額")]
+        public int TotalPrice { get; set; }
     }
 }
